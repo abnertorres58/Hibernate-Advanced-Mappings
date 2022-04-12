977d000c2adc9b080204dd995feb420f16ebf437
fred <abnertorres58@gmail.com>
2022-04-12 17:46:25 -0400

    One to One Bi Directional Cascade Delete

---
+++ src/com/freudromero/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,58 @@
+package com.freudromero.hibernate.demo;
+
+import com.freudromero.hibernate.demo.entity.Instructor;
+import com.freudromero.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class DeleteInstructorDetailDemo {
+
+    public static void main(String[] args) {
+
+        // Create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // Create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // Start a transaction
+            session.beginTransaction();
+
+            // Get the instructor detail object
+            int theId = 2;
+            InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+
+            // Print the instructor detail
+            System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+
+            // Print the associated instructor
+            System.out.println("The associated instructor: " + tempInstructorDetail.getInstructor());
+
+            // Now let's delete the instructor detail
+            System.out.println("Deleting temInstructorDetail: " + tempInstructorDetail);
+            session.delete(tempInstructorDetail);
+
+            // Commit the transaction
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+        finally {
+            // Handle connection leak issue
+            session.close();
+
+            factory.close();
+        }
+
+    }
+}
