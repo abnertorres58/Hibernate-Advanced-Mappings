a88c4973accaedbfa1ee85cf2e638e7d61a16f85
fred <abnertorres58@gmail.com>
2022-04-12 20:46:55 -0400

    OneToMany Bidirectional Update Instructor

--- src/com/freudromero/hibernate/demo/entity/Instructor.java
+++ src/com/freudromero/hibernate/demo/entity/Instructor.java
@@ -1,6 +1,8 @@
 package com.freudromero.hibernate.demo.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "instructor")
@@ -37,6 +39,9 @@ public class Instructor {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
+
+    @OneToMany(mappedBy = "instructor", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    private List<Course> courses;
 
     public Instructor() {
     }
@@ -85,6 +90,25 @@ public class Instructor {
 
     public void setInstructorDetail(InstructorDetail instructorDetail) {
         this.instructorDetail = instructorDetail;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    // Add convenience methods for bidirectional relationship
+    public void add(Course tempCourse) {
+        if(courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+
+        tempCourse.setInstructor(this);
     }
 
     @Override
