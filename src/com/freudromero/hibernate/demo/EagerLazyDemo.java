0fec93726a54e30507a73480e0e3d7fe0032d3b8
fred <abnertorres58@gmail.com>
2022-04-13 14:50:46 -0400

    Eager vs Lazy Loading - Coding - Eager

---
+++ src/com/freudromero/hibernate/demo/EagerLazyDemo.java
@@ -0,0 +1,52 @@
+package com.freudromero.hibernate.demo;
+
+import com.freudromero.hibernate.demo.entity.Course;
+import com.freudromero.hibernate.demo.entity.Instructor;
+import com.freudromero.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class EagerLazyDemo {
+
+    public static void main(String[] args) {
+
+        // Create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        // Create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // Start a transaction
+            session.beginTransaction();
+
+            // Get the instructor from DB
+            int theId = 2;
+            Instructor tempInstructor = session.get(Instructor.class, theId);
+
+            System.out.println("freudromero: Instructor: " + tempInstructor);
+
+            // Get courses for the instructor
+            System.out.println("freudromero: Instructor: " + tempInstructor.getFirstName() + " " + tempInstructor.getLastName() +" Courses: " + tempInstructor.getCourses());
+
+
+            // Commit the transaction
+            session.getTransaction().commit();
+            System.out.println("freudromero: Done!");
+        }
+        finally {
+            // Add clean up code
+            session.close();
+            factory.close();
+        }
+
+    }
+}
--- src/com/freudromero/hibernate/demo/entity/Instructor.java
+++ src/com/freudromero/hibernate/demo/entity/Instructor.java
@@ -40,7 +40,9 @@ public class Instructor {
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
 
-    @OneToMany(mappedBy = "instructor", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @OneToMany(fetch = FetchType.EAGER,
+            mappedBy = "instructor",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
     private List<Course> courses;
 
     public Instructor() {
